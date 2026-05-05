eaf5cd22c5296775433699bda179cfd0c9ba94da
Arga Bayu <argabayu.rmdhn0@gmail.com>
2026-05-05 12:49:48 +0700

    feat: implement CRUD operations for sekolah module via controller and service layers

---
+++ backend-ppdb/src/services/sekolah.service.ts
@@ -0,0 +1,36 @@
+import prisma from "../config/prisma"
+
+type SekolahInput = {
+    nama: string;
+    alamat: string;
+    kuota: number;
+}
+
+export const createSekolah = async (data: SekolahInput) => {
+    return await prisma.sekolah.create({ data });
+}
+
+export const getAllSekolah = async () => {
+    return await prisma.sekolah.findMany({
+        orderBy: { createAt: "desc"},
+    });
+};
+
+export const getSekolahById = async (id: string) => {
+    return await prisma.sekolah.findUnique({ 
+        where: {id},
+    });
+};
+
+export const updateSekolah = async (id: string, data: SekolahInput) => {
+    return await prisma.sekolah.update({
+        where: {id}, 
+        data,
+    });
+};
+
+export const deleteSekolah = async (id: string) => {
+    return await prisma.sekolah.delete({
+        where: {id},
+    });
+};
