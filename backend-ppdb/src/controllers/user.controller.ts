36ef1863200d81606b882287f30aefee8e57b768
Erwin Gutawa <erwinmyboy03@gmail.com>
2026-05-12 15:01:08 +0700

    add biodata and sekolah integration api

--- backend-ppdb/src/controllers/user.controller.ts
+++ backend-ppdb/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import prisma from "../config/prisma";
 import { getPengumumanByUser } from "../services/user.service";
 
 export const getPengumuman = async (req: any, res: Response) => {
@@ -9,6 +10,42 @@ export const getPengumuman = async (req: any, res: Response) => {
 
     res.json({ data });
   } catch (error) {
-    res.status(500).json({ message: "Terjadi kesalahan" });
+    res.status(500).json({
+      message: "Terjadi kesalahan",
+    });
+  }
+};
+
+export const updateBiodata = async (req: any, res: Response) => {
+  try {
+    const userId = req.user.id;
+
+    const { alamat, kelurahan, kecamatan, noTlpn, latitude, longitude } =
+      req.body;
+
+    const user = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        alamat,
+        kelurahan,
+        kecamatan,
+        noTlpn,
+        latitude,
+        longitude,
+      },
+    });
+
+    return res.status(200).json({
+      message: "Biodata berhasil disimpan",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Terjadi kesalahan server",
+    });
   }
 };
