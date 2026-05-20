6fd7827fc2fd33cb355c60865f1e4966a9361158
Erwin Gutawa <erwinmyboy03@gmail.com>
2026-05-20 15:20:12 +0700

    benerin fitur upload

--- backend-ppdb/src/middlewares/upload.middleware.ts
+++ backend-ppdb/src/middlewares/upload.middleware.ts
@@ -1,31 +1,12 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
-
-const uploadPath = "/tmp/uploads";
-
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, {
-    recursive: true,
-  });
-}
-
-const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => {
-    cb(null, uploadPath);
-  },
-
-  filename: (_req, file, cb) => {
-    const uniqueName = `${Date.now()}${path.extname(file.originalname)}`;
-    cb(null, uniqueName);
-  },
-});
 
 export const upload = multer({
-  storage,
+  storage: multer.memoryStorage(),
+
   limits: {
     fileSize: 5 * 1024 * 1024,
   },
+
   fileFilter: (_req, file, cb) => {
     const allowed = ["application/pdf", "image/jpeg", "image/png"];
 
